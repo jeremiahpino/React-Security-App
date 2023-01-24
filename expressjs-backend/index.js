44ef581f3582ca8b84dd8069b05a61df2a47fe9b
jeremiahpino <jpino1999@gmail.com>
2023-01-24 11:45:20 -0800

    installed axios cors nodemon

--- expressjs-backend/index.js
+++ expressjs-backend/index.js
@@ -2,14 +2,50 @@ const express = require('express');
 const app = express();
 const port = 5001;
 
+const cors = require('cors');
+
+app.use(cors());
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('This is JPs App');
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });   
 
-// make a route 
+// list of current users 
+const users = {
+    users_list : [
+        {
+            username : "bob",
+            password : "noPass"
+        }
+    ]
+}
+
+
+
+// make a post route 
+app.post('/login', (req, res) => {
+
+    // uName = req.body.username;
+    // pWord = req.body.password;
+
+    // if ( ("jpino" === uName) && ("fakePass" === pWord)) {
+
+    // }
+
+    if(req.body.password === "fakePass") {
+
+        res.status(201).end();
+    }
+    else {
+        res.status(404).end();
+    }
+
+    
+
+});
--- react-frontend/src/Home.js
+++ react-frontend/src/Home.js
@@ -1,7 +1,8 @@
-// step 6
-// had to add onLogin 
+// imports 
 import { useAuth } from "./context/AuthProvider";
 import { useState } from "react";
+
+import axios from 'axios';
 
 export const Home = () => {
   const { value } = useAuth();
@@ -26,6 +27,27 @@ export const Home = () => {
     }
   }
 
+  // create a aync function to login someone
+  // pass in use states and make structure 
+  async function makePostCall(person) {
+    try {
+
+      // save response and return to frontend 
+      const response = await axios.post('http://localhost:5001/login', person)
+
+      return response;
+    }
+    catch (error) {
+
+      // print out error if error occurs
+      console.log(error);
+
+      return false;
+
+    }
+  }
+
+
   return (
     <>
       <h2>Home (Public)</h2>
@@ -45,6 +67,7 @@ export const Home = () => {
         onChange={handlePasswordChange}
         ></input>
 
+      {/* pass in makePostCall */}
       <button type="button" onClick={handleClick}>
         Sign In
       </button>
